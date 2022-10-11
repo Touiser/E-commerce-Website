941bc7fbdde8611ae55260c2f8f880bac4db6f58
Mahdi Touiser <mahditouiserkani@yahoo.com>
2022-10-11 08:21:54 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,637 @@
+// wholeMenu
+const wholeMenu = [
+  {
+    //کالای دیجیتال
+    title: "کالای دیجیتال",
+    submenu: [
+      {
+        title: "لوازم جانبی گوشی",
+        submenu: [
+          {
+            title: "کیف  و کاور گوشی",
+          },
+          {
+            title: " پاوربانک و شارژر  ",
+          },
+          {
+            title: "پایه نگهدارنده گوشی",
+          },
+        ],
+      },
+      {
+        title: "دوربین",
+        submenu: [
+          { title: "دوربین عکاسی دیجیتال" },
+          { title: "دوربین ورزشی و فیلمبرداری " },
+          { title: "دوربین چاپ سریع " },
+        ],
+      },
+      { title: "لوازم جانبی دوربین" },
+
+      {
+        title: "گوشی موبایل",
+        submenu: [
+          { title: "اپل" },
+          { title: "سامسونگ" },
+          { title: "هواوی" },
+          { title: "شیائومی" },
+          { title: "نوکیا" },
+        ],
+      },
+
+      { title: "مچ بند و ساعت هوشمند " },
+      { title: " هدفون ، هدست و هندزفری " },
+      { title: " اسپیکر بلوتوث و با سیم" },
+      { title: "  واقعیت مجازی" },
+      { title: "تلسکوپ" },
+      { title: "کامپیوتر و لوازم جانبی" },
+      { title: "لپتاپ" },
+      { title: "لوازم جانبی لپتاپ" },
+      { title: "هارد ، فلش و SSD" },
+    ],
+  },
+
+  {
+    //کالا های سوپرمارکتی
+    title: "کالاهای سوپرمارکتی ",
+    submenu: [
+      {
+        title: "کالای اساسی و خوار و بار",
+        submenu: [
+          { title: " نان " },
+          { title: " برنج " },
+          { title: " روغن " },
+          { title: " قند " },
+          { title: " شکر " },
+          { title: " سس " },
+          { title: " رب و کنسرو گوجه " },
+          { title: "خیارشور و ترشیجات" },
+        ],
+      },
+      {
+        title: "لبنیات ",
+        submenu: [
+          { title: "شیر" },
+          { title: "دوغ" },
+          { title: "پنیر" },
+          { title: "کره" },
+        ],
+      },
+      { title: "صبحانه " },
+      { title: "مواد پروتئینی " },
+      { title: "میوه و سبزی  " },
+      { title: " خشکبار و شیرینی" },
+      { title: "کنسرو و کمپوت" },
+      { title: "تنقلات" },
+      { title: "نوشیدنی ها" },
+    ],
+  },
+  //مد و پوشاک
+  {
+    title: "مد و پوشاک ",
+    submenu: [
+      {
+        title: "لباس مردانه",
+        submenu: [
+          { title: "تی شرت" },
+          { title: "پیراهن  " },
+          { title: "شلوار  " },
+          { title: "لباس زیر  " },
+        ],
+      },
+      {
+        title: "کفش مردانه ",
+        submenu: [{ title: "کفش روزمره" }, { title: "کفش رسمی" }],
+      },
+
+      { title: " لباس زنانه " },
+      { title: "اکسسوری زنانه " },
+      { title: "زیورآلات طلا زنانه " },
+      { title: "زیورآلات  نقره زنانه" },
+      { title: "عینک آفتابی مردانه" },
+      { title: "عینک آفتابی زنانه" },
+      { title: "کفش  ورزشی مردانه" },
+      { title: "کفش  ورزشی زنانه" },
+      { title: "کفش  ورزشی پسرانه" },
+      { title: "کفش  ورزشی دخترانه" },
+    ],
+  },
+
+  // //کتاب ، لوازم التحریر و هنر
+  {
+    title: "کتاب ، لوازم التحریر و هنر ",
+    submenu: [
+      {
+        title: "صنایع دستی",
+        submenu: [
+          { title: " کالاهای مسی  " },
+          { title: "  سفال ، سرامیک و چینی  " },
+          { title: " کیف چرمی   " },
+          { title: " ترمه ، قلمکار و دستبافت   " },
+          { title: " تابلو و ساعت  " },
+          { title: " میناکاری  " },
+          { title: " فیروزه کوبی   " },
+          { title: " سوزن دوزی   " },
+          { title: " دست سازه های هنری   " },
+        ],
+      },
+      {
+        title: "کتاب و مجله ",
+        submenu: [
+          { title: "مجلات داخلی  " },
+          { title: "مجلات خارجی  " },
+          { title: "کتاب چاپی" },
+        ],
+      },
+
+      {
+        title: "محتوای آموزشی ",
+        submenu: [
+          { title: " آموزش موسیقی" },
+          { title: " آموزش ورزش و سرگرمی" },
+          { title: " آموزش زبان " },
+          { title: " آموزش نرم افزار و کامپیوتر " },
+        ],
+      },
+      {
+        title: "آلات موسیقی ",
+        submenu: [
+          { title: "گیتار " },
+          { title: "کیبورد و ارگ " },
+          { title: " ویولن " },
+          { title: " پیانو دیجیتال " },
+          { title: "ساز های ایرانی " },
+        ],
+      },
+      {
+        title: "لوازم التحریر  ",
+        submenu: [
+          { title: "نوشت افزار " },
+          { title: "دفتر و کاغذ" },
+          { title: "خودکار و روان نویس" },
+          { title: "میز تحریر" },
+          { title: "آلبوم عکس " },
+          { title: " مداد و مداد رنگی " },
+          { title: " میز تحریر" },
+        ],
+      },
+
+      {
+        title: " نرم افزار",
+      },
+      {
+        title: "فرش ماشینی ، دستبافت ، تابلو  ",
+        submenu: [
+          { title: " فرش ماشینی " },
+          { title: " فرش دستبافت " },
+          { title: " تابلوفرش  " },
+        ],
+      },
+      {
+        title: " بازی کنسول و کامپیوتر ",
+      },
+      {
+        title: " فیلم سینمایی ، سریال و مستند ",
+      },
+    ],
+  },
+  //اسباب بازی کودک
+  {
+    title: "اسباب بازی و کودک",
+    submenu: [
+      {
+        title: " بهداشت و حمام کودک و نوزاد",
+        submenu: [
+          { title: " حوله " },
+          { title: " وان حمام نوزاد   " },
+          { title: " مینی واش   " },
+          { title: " شامپو کودک و نوزاد   " },
+        ],
+      },
+      {
+        title: "پوشاک و کفش کودک و نوزاد",
+        submenu: [
+          { title: " پوشاک  " },
+          // { title: " دستمال مرطوب   " },
+          { title: " لباس کودک و لباس نوزادی   " },
+          { title: " کفش" },
+        ],
+      },
+      {
+        title: " اسباب بازی",
+        submenu: [
+          { title: " فکری و آموزشی " },
+          { title: "  پازل ، لگو و ساختنی " },
+          { title: " عروسک و فیگور " },
+          { title: " اسپینر ، ابزار شوخی و سرگرمی " },
+        ],
+      },
+      {
+        title: "  بازی و سرگرمی کودک",
+        submenu: [
+          { title: " ماشین بازی ، موتور ، سه چرخه " },
+          { title: "  دوچرخه  " },
+          { title: "  تشک بازی  " },
+          { title: "  تاپ و سرسره  " },
+        ],
+      },
+    ],
+  },
+  //محصولات بومی و محلی
+  {
+    title: " محصولات بومی و محلی",
+    submenu: [
+      {
+        title: " خوراکی های بومی و محلی ",
+        submenu: [
+          { title: "  خرمای محلی" },
+          { title: "  عسل محلی " },
+          { title: "  عرقیات و گلاب اصیل" },
+          { title: "  ادویه و چاشنی محلی " },
+          { title: "  چای  محلی " },
+          { title: "  روغن محلی " },
+          { title: "  سبزی خشک محلی " },
+          { title: "  ماهی تازه " },
+        ],
+      },
+      {
+        title: " ظروف سنتی",
+        submenu: [
+          { title: "  بشقاب سنتی   " },
+          { title: "   پارچ سنتی   " },
+          { title: "   لیوان سنتی   " },
+          { title: "   قندان سنتی   " },
+          { title: "   دیگه و قابلمه سنتی   " },
+        ],
+      },
+      {
+        title: "  پوشیدنی های بومی محلی",
+        submenu: [{ title: "  پوشاک بومی و محلی   " }],
+      },
+    ],
+  },
+  //خودرو ، ابزار و تجهیزات صنعتی
+  {
+    title: " خودرو ، ابزار و تجهیزات صنعتی",
+    submenu: [
+      {
+        title: "    لوازم جانبی خودرو و موتورسیکلت",
+        submenu: [
+          { title: "  لوازم تزیینی   " },
+          { title: "  سیستم صوتی و تصویری    " },
+          { title: "  نظافت و نگهداری خودرو    " },
+          { title: "  کلاه کاسکت و لوازم جانبی موتور  " },
+          { title: "  کلاه کاسکت و لوازم جانبی موتور  " },
+          { title: "  دیسک و صفحه کلاچ" },
+          { title: "   جلوبندی و تعلیق " },
+          { title: "   چراغ خودرو " },
+          { title: "   تسمه خودرو " },
+          // { title: " کمک فنر" },
+        ],
+      },
+      { title: "  خودرو های ایرانی و خارجی" },
+      {
+        title: "  موتورسیکلت",
+      },
+    ],
+  },
+  {
+    title: "زیبایی و سلامت",
+    submenu: [
+      {
+        title: "لوازم آرایشی",
+        submenu: [
+          { title: "آرایش چشم و ابرو" },
+          { title: "آرایش  لب" },
+          { title: "آرایش  صورت" },
+          { title: "   مواد آرایش مو" },
+        ],
+      },
+      {
+        title: "لوازم بهداشتی",
+        submenu: [
+          { title: "کرم مراقبت پوست" },
+          { title: "شامپو و مراقبت پوست" },
+          { title: "بهداشت دهان و دندان " },
+          { title: " بهذاشت و مراقبت بدن" },
+          { title: " ضد تعریق " },
+        ],
+      },
+      {
+        title: " عطر ، ادکلن ، اسپری و ست",
+        submenu: [
+          { title: "مردانه" },
+          { title: " زنانه" },
+          { title: " جیبی" },
+          { title: " اسپری  " },
+        ],
+      },
+
+      {
+        title: "ابزار سلامتی و طبی ",
+        submenu: [
+          { title: "ترازو " },
+          { title: "مچ بند و ساعت هوشمند " },
+          { title: " تست قند خون " },
+          { title: "  تب سنج " },
+          { title: " فشار سنج  " },
+        ],
+      },
+    ],
+  },
+  {
+    title: "خانه و آشپزخانه",
+    submenu: [
+      {
+        title: "نور و روشنایی",
+        submenu: [
+          { title: " لامپ ، چراغ و ریسه  " },
+          { title: " لوستر و چراغ تزیینی " },
+        ],
+      },
+      {
+        title: " دکوراتیو",
+        submenu: [
+          { title: "مبلمان خانگی " },
+          { title: " آینه" },
+          { title: " پرده" },
+          { title: " تابلو " },
+          { title: " دکوراسیون اداری  " },
+        ],
+      },
+      {
+        title: "صوتی و تصویری",
+        submenu: [
+          { title: "تلویزیون " },
+          { title: " سینمای خانگی و ساندبار" },
+          { title: " گیرنده دیجیتال تلویزیون " },
+        ],
+      },
+      {
+        title: " لوازم برقی خانگی",
+        submenu: [
+          { title: "یخچال و فریزر  " },
+          { title: " ماشین لباسشویی" },
+          { title: " ماشین ظرفشویی" },
+          { title: " هود " },
+          { title: " اجاق گاز " },
+        ],
+      },
+    ],
+  },
+  {
+    title: " ورزش و سفر ",
+    submenu: [
+      {
+        title: " کوهنوردی و کمپینگ",
+        submenu: [
+          { title: "کفش کوهنوردی " },
+          { title: " عصای کوهنوردی   " },
+          { title: " قمقمه و فلاسک   " },
+          { title: "  چادر  " },
+          { title: "  کیسه خواب   " },
+        ],
+      },
+      {
+        title: "   ورزش های توپی",
+        submenu: [{ title: " توپ " }, { title: " راکت " }],
+      },
+      {
+        title: " ورزش های هوازی و بدنسازی",
+        submenu: [
+          { title: " تجهیزات جانبی تناسب اندام " },
+          { title: " دمبل" },
+          { title: " طناب" },
+          { title: " بارفیکس " },
+          { title: " تردمیل   " },
+        ],
+      },
+
+      {
+        title: "   ورزش های آبی",
+      },
+      {
+        title: " ورزش های رزمی ",
+      },
+    ],
+  },
+];
+//products
+const products = [
+  // موبایل
+  {
+    productName: " آیفون 13 پرو مکس",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/13promax.jpg",
+  },
+  {
+    productName: "آیفون 13 پرو",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/13pro.jpg",
+  },
+  {
+    productName: "آیفون 13",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/13.jpg",
+  },
+  {
+    productName: "آیفون 13 مینی",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/13mini.jpg",
+  },
+  {
+    productName: " آیفون 12 پرو مکس",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/12promax.jpg",
+  },
+  {
+    productName: "آیفون 12 پرو",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/12pro.jpg",
+  },
+  {
+    productName: "آیفون 12",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/12.jpg",
+  },
+  {
+    productName: "آیفون 11 پرو مکس",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/11promax.jpg",
+  },
+  {
+    productName: "آیفون 11 پرو",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/11pro.jpg",
+  },
+  {
+    productName: "آیفون 11",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/11.jpg",
+  },
+  {
+    productName: "آیفون SE 2022",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/SE2022.jpg",
+  },
+  {
+    productName: "آیفون SE 2020",
+    category: "گوشی موبایل",
+    image: "./Items/Phones/SE2020.jpg",
+  },
+  {
+    productName: "iphone - آیفون",
+    category: "  گوشی موبایل ",
+  },
+  {
+    productName: " آیفون تصویری ",
+    category: "در باز کن",
+  },
+  {
+    productName: "کاور آیفون",
+    category: "کیف و کاور گوشی",
+  },
+];
+
+let ul = document.getElementById("unOrderedList");
+for (let i = 0; i < wholeMenu.length; i++) {
+  div = document.getElementById("myDropdown");
+  let output = wholeMenu[i];
+  let li = document.createElement("li");
+  li.id = "menu-" + i;
+  li.addEventListener("mouseover", hoverOn);
+  ul.append(li);
+  li.append(output.title);
+}
+
+let menu = document.getElementById("dropSearchBar");
+let dropdownMenu = document.getElementById("myDropdown");
+let bgDropdownMenu = document.getElementById("bgDropdownMenu");
+
+function dropSearchBar() {
+  menu.style.opacity = "1";
+  menu.style.visibility = "visible";
+  menu.style.transition = "all 0.3s";
+  bgDropdownMenu.classList.add("bgDropdownMenu");
+}
+
+function closeSearchBar() {
+  menu.style.opacity = "0";
+  menu.style.visibility = "hidden";
+  menu.style.transition = "all 0.3s";
+  bgDropdownMenu.classList.remove("bgDropdownMenu");
+}
+
+function hoverOn(e) {
+  const menuHtmlId = e.target.id;
+  const menuId = +menuHtmlId.split("-")[1];
+  const childId = menuId;
+  const div = document.getElementById("dropdown-content-under");
+
+  div.innerHTML = "";
+  for (let i = 0; i < wholeMenu[menuId].submenu.length; i++) {
+    let liOne = document.createElement("li");
+    let span = document.createElement("span");
+    let outputOne = wholeMenu[menuId].submenu[i];
+    let ulOne = document.createElement("ul");
+    span.classList.add("firstUl");
+    div.append(ulOne);
+    ulOne.append(liOne);
+    liOne.append(span);
+    span.append(outputOne.title);
+    const submenu = wholeMenu[childId].submenu[i];
+    let ulTwo = document.createElement("ul");
+    if (submenu.submenu) {
+      for (let j = 0; j < submenu.submenu.length; j++) {
+        let liTwo = document.createElement("li");
+        liTwo.classList.add("secondUl");
+        let outputTwo = wholeMenu[childId].submenu[i].submenu[j];
+        liOne.append(ulTwo);
+        ulTwo.append(liTwo);
+        liTwo.append(outputTwo.title);
+      }
+    }
+  }
+  div.style.display = "flex";
+}
+
+function dropBtn() {
+  dropdownMenu.style.opacity = "1";
+  dropdownMenu.style.visibility = "visible";
+  dropdownMenu.style.transition = "all 0.3s";
+  bgDropdownMenu.classList.add("bgDropdownMenu");
+}
+function OutOfBtn() {
+  dropdownMenu.style.opacity = "0";
+  dropdownMenu.style.visibility = "hidden";
+  dropdownMenu.style.transition = "all 0.3s";
+  bgDropdownMenu.classList.remove("bgDropdownMenu");
+}
+function openNav() {
+  document.getElementById("mySidebar").style.width = "200px";
+  document.getElementById("form-control").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.display = "block";
+}
+
+function doSearch(event) {
+  event.preventDefault();
+
+  let searchBox = document.getElementById("searchInput").value;
+  searchBox = searchBox.trim();
+  const div = document.getElementById("searchBox");
+  const imageDiv = document.getElementById("imageSearchBox");
+  const divCategory = document.getElementById("searchCategory");
+  const suggestedDiv = document.getElementById("suggestedSearch");
+  const suggestedSearchItemsContainer = document.getElementById(
+    "suggestedSearchItemsContainer"
+  );
+  div.innerHTML = "";
+  divCategory.innerHTML = "";
+  imageDiv.innerHTML = "";
+  suggestedDiv.innerHTML = "";
+  suggestedSearchItemsContainer.innerHTML = "";
+
+  for (let i = 0; i < products.length; i++) {
+    const innerDiv = document.createElement("div");
+    const paragraph = document.createElement("p");
+    const match = products[i].productName;
+    if (
+      searchBox !== "" &&
+      match.indexOf(searchBox) > -1 &&
+      products[i].image
+    ) {
+      const matchImage = products[i].image;
+      const img = document.createElement("img");
+      img.src = matchImage;
+      innerDiv.appendChild(img);
+      div.append(innerDiv);
+      innerDiv.append(paragraph);
+      paragraph.append(match);
+    } else if (
+      searchBox !== "" &&
+      match.indexOf(searchBox) > -1 &&
+      !products[i].image
+    ) {
+      const innerDivCategory = document.createElement("div");
+      const innerDivCategoryIcon = document.createElement("div");
+      const Category = document.createElement("p");
+      const Title = document.createElement("p");
+      const matchTitle = products[i].productName;
+      const matchCategory = products[i].category;
+
+      divCategory.append(innerDivCategory);
+      innerDivCategory.appendChild(innerDivCategoryIcon);
+      innerDivCategory.appendChild(Title);
+      innerDivCategory.appendChild(Category);
+      Title.append(matchTitle);
+      innerDivCategoryIcon.innerHTML =
+        '<i class="fa-solid fa-magnifying-glass"></i>';
+      Category.append("در دسته" + " " + matchCategory);
+    }
+  }
+}
